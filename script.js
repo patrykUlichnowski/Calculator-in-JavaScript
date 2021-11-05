dc4196dca77d591f7a86e5533593ca83257916a7
Patryk Ulichnowski <82456478+patrykUlichnowski@users.noreply.github.com>
2021-11-05 21:00:38 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,61 +2,73 @@
   Simple JavaScript calculator made by Patryk Ulichnowski.
 */
 
-let sign = '';
-let screen = document.getElementById('screen');
+let sign = "";
+let screen = document.getElementById("screen");
 let userInputNumber = 0;
 let memorizedNumber = undefined;
 let result = 0;
 const wasSolved = false;
 
 function addListeners() {
-  let numberArray = document.querySelectorAll('#number');
-  let operatorsArray = document.querySelectorAll('#operator');
+  let numberArray = document.querySelectorAll("#number");
+  let operatorsArray = document.querySelectorAll("#operator");
   for (let i = 0; i < numberArray.length; i++) {
     if (i == 9) {
       numberArray[i].numberID = 0;
     } else {
       numberArray[i].numberID = i + 1;
     }
-    numberArray[i].addEventListener('click', userInput);
+    numberArray[i].addEventListener("click", userInput);
   }
   for (let i = 0; i < operatorsArray.length; i++) {
     // Here we add what type of sign to use
     switch (i) {
       case 0:
-        operatorsArray[i].sign = '+'; break;
+        operatorsArray[i].sign = "+";
+        break;
       case 1:
-        operatorsArray[i].sign = '-'; break;
+        operatorsArray[i].sign = "-";
+        break;
       case 2:
-        operatorsArray[i].sign = '*'; break;
+        operatorsArray[i].sign = "*";
+        break;
       case 3:
-        operatorsArray[i].sign = '/'; break;
+        operatorsArray[i].sign = "/";
+        break;
       case 4:
-        operatorsArray[i].sign = 'sqrt'; break;
+        operatorsArray[i].sign = "sqrt";
+        break;
       case 5:
-        operatorsArray[i].sign = 'C'; break;
+        operatorsArray[i].sign = "C";
+        break;
       case 6:
-        operatorsArray[i].sign = '='; break;
+        operatorsArray[i].sign = "=";
+        break;
     }
-    operatorsArray[i].addEventListener('click', calculation)
+    operatorsArray[i].addEventListener("click", calculation);
   }
-
 }
-
-function userInput() {
+let exist = false; // TUTAJ
+function userInput(event) {
   if (userInputNumber == 0) {
     userInputNumber = String(event.target.numberID);
   } else {
     userInputNumber += String(event.target.numberID);
   }
   screen.innerHTML = userInputNumber;
+  if (memorizedNumber !== undefined && exist && result !== 0) {
+    // TUTAJ
+    console.log("jest"); // TUTAJ
+    memorizedNumber = userInputNumber; // TUTAJ
+  }
 }
-
-function calculation() {
+setInterval(() => console.log(memorizedNumber, userInputNumber, exist), 1000);
+function calculation(event) {
+  exist = true; // TUTAJ
   let triger = String(event.target.sign); // Grabs what type of operator caused function
-  if (triger !== '=' || triger === 'sqrt' || triger === 'C') {
+  if (triger !== "=" || triger === "sqrt" || triger === "C") {
     // Square root
-    if (triger === 'sqrt') {
+    if (triger === "sqrt") {
       if (memorizedNumber === undefined) {
         // Square root for first number
         screen.innerHTML = Math.sqrt(Number(userInputNumber));
@@ -67,7 +79,7 @@ function calculation() {
         memorizedNumber = Math.sqrt(Number(memorizedNumber));
       }
     } else {
-      if (triger === 'C') {
+      if (triger === "C") {
         // clean option
         screen.innerHTML = 0;
         result = 0;
@@ -75,33 +87,43 @@ function calculation() {
         userInputNumber = 0;
       } else {
         // This part is executed when operator is different than '='
-        if (memorizedNumber === undefined || memorizedNumber === userInputNumber) {
+        if (
+          memorizedNumber === undefined ||
+          memorizedNumber === userInputNumber ||
+          exist // TUTAJ
+        ) {
           memorizedNumber = Number(userInputNumber);
+          exist = false; // TUTAJ
         }
         userInputNumber = 0;
         sign = triger;
       }
     }
-  }
-  else {
+  } else {
     switch (sign) {
       // Here are just calculations
-      case '+':
-        result = Number(Number(memorizedNumber) + Number(userInputNumber)); break;
-      case '-':
-        result = Number(Number(memorizedNumber) - Number(userInputNumber)); break;
-      case '*':
-        result = Number(Number(memorizedNumber) * Number(userInputNumber)); break;
-      case '/':
-        result = Number(Number(memorizedNumber) / Number(userInputNumber)); break;
-      case '=':
-        result = Number(memorizedNumber); break;
+      case "+":
+        result = Number(Number(memorizedNumber) + Number(userInputNumber));
+        break;
+      case "-":
+        result = Number(Number(memorizedNumber) - Number(userInputNumber));
+        break;
+      case "*":
+        result = Number(Number(memorizedNumber) * Number(userInputNumber));
+        break;
+      case "/":
+        result = Number(Number(memorizedNumber) / Number(userInputNumber));
+        break;
+      case "=":
+        result = Number(memorizedNumber);
+        break;
     }
     screen.innerHTML = result;
     memorizedNumber = result; // We pass the result to mem so user can do operations on result
     userInputNumber = 0;
-    console.log('mem ' + memorizedNumber);
-    console.log('us ' + userInputNumber);
-    console.log('sum ' + result);
+    exist = false; // TUTAJ
+    console.log("mem " + memorizedNumber);
+    console.log("us " + userInputNumber);
+    console.log("sum " + result);
   }
 }
